c4a5e59f57dfcbf155b09fd5c586b47d1fc3a53d
Alexk-AG <pepechuy5002@gmail.com>
2025-03-25 01:12:02 -0600

    Update ALU.v

--- ALU.v
+++ ALU.v
@@ -1,23 +1,17 @@
-module ALU(
-    input  [31:0] data1,     
-    input  [31:0] data2,      
-    input  [3:0] op,  
+module ALU (
+    input wire [31:0] data1, data2,
+    input wire [3:0] alucode,
     output reg [31:0] dataOut
 );
-
     always @(*) begin
-        case (op)
-            4'b0000: dataOut = data1 & data2;		   
-            4'b0001: dataOut = data1 | data2;      
-            4'b0010: dataOut = data1 + data2;      
-            4'b0110: dataOut = data1 - data2;        
-			4'b0111: dataOut = data1 < data2 ? 1 : 0;
-			4'b1100: dataOut = ~(data1 | data2);
-            default: dataOut = 32'b0;     
+        case (alucode)
+            4'b0000: dataOut = data1 & data2;
+            4'b0001: dataOut = data1 | data2;
+            4'b0010: dataOut = data1 + data2;
+            4'b0110: dataOut = data1 - data2;
+            4'b0111: dataOut = (data1 < data2) ? 32'd1 : 32'd0;
+            4'b1100: dataOut = ~(data1 | data2);
+            default: dataOut = 32'b0;
         endcase
     end
-
 endmodule
-
-// ARITM = opcode, Registerdestino, Rsource1, Rsource2
-// Sw = opcode, x. Dirección, dato1
